4a1f5901f40adb98945f64ee6183e33be1706bbe
semenak94 <semenak94@mail.ru>
2017-11-23 01:11:24 +0300

    Tweaks with configuration settings. High precision configuration added.

--- src/include/BasicTypes.h
+++ src/include/BasicTypes.h
@@ -11,18 +11,23 @@
 
 #ifdef HIGH_PRECISION
 using BmpReal = boost::multiprecision::cpp_dec_float_50;
+namespace fdsf {
+    // Желаемая точность расчета
+    //const BmpReal epsilon = 1e-45;
+    const BmpReal epsilon = 1e-17;
+}
 #else
 using BmpReal = double;
+namespace fdsf {
+    // Желаемая точность расчета
+    const BmpReal epsilon = 1e-17;
+}
 #endif
 
 using BmpVector = std::vector<BmpReal>;
 using BmpMatrix = std::vector<BmpVector>;
 
 namespace fdsf {
-    // Желаемая точность расчета
-    //const BmpReal epsilon = 1e-45;
-    const BmpReal epsilon = 1e-17;
-
     // Значение pi
     const BmpReal PI = boost::math::constants::pi<BmpReal>();
 }
