e07205c2bac4742f95d9bad9d229cf645ccf5ee3
semenak94 <semenak94@mail.ru>
2018-11-08 01:42:56 +0300

    Встроено вычисление интегральной ФД всюду сходящимся рядом

--- src/fdsf/src/include/FullyConvergedSeries.h
+++ src/fdsf/src/include/FullyConvergedSeries.h
@@ -3,6 +3,9 @@
 #include "BasicTypes.h"
 
 namespace fcs {
-    // Вычислить значение ФД для x <= 0
+    // Вычислить значение ФД индекса k для x <= 0
     BmpReal calculate(BmpReal k, BmpReal x);
+
+    // Вычислить значение интегральной ФД для x <= 0
+    BmpReal calculateJmhalf(BmpReal x);
 }
