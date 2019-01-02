0f9689161db18020f0e342e1fce3cc1a9c1da687
semenak94 <semenak94@mail.ru>
2019-01-02 23:00:17 +0300

    Исправлена сборка теста

--- src/fdsf/test/src/unit-constants.cpp
+++ src/fdsf/test/src/unit-constants.cpp
@@ -1,4 +1,5 @@
 #include "Common.h"
+#include "ZetaFunction.h"
 
 TEST_CASE("bernulli") {
     SECTION("odd") {
@@ -33,31 +34,31 @@ TEST_CASE("bernulli") {
 
 TEST_CASE("dzeta") {
     SECTION("odd") {
-        CHECK(0 == dzetaFunction(1));
-        CHECK(0 == dzetaFunction(3));
-        CHECK(0 == dzetaFunction(5));
-        CHECK(0 == dzetaFunction(7));
-        CHECK(0 == dzetaFunction(9));
-        CHECK(0 == dzetaFunction(11));
-        CHECK(0 == dzetaFunction(13));
-        CHECK(0 == dzetaFunction(15));
-        CHECK(0 == dzetaFunction(17));
-        CHECK(0 == dzetaFunction(19));
-        CHECK(0 == dzetaFunction(21));
-        CHECK(0 == dzetaFunction(23));
+        CHECK(0 == zetaFunction(1));
+        CHECK(0 == zetaFunction(3));
+        CHECK(0 == zetaFunction(5));
+        CHECK(0 == zetaFunction(7));
+        CHECK(0 == zetaFunction(9));
+        CHECK(0 == zetaFunction(11));
+        CHECK(0 == zetaFunction(13));
+        CHECK(0 == zetaFunction(15));
+        CHECK(0 == zetaFunction(17));
+        CHECK(0 == zetaFunction(19));
+        CHECK(0 == zetaFunction(21));
+        CHECK(0 == zetaFunction(23));
     }
     SECTION("even") {
-        CHECK(_1 / 6 == dzetaFunction(2) / pow(pi(), 2));
-        CHECK(_1 / 90 == dzetaFunction(4) / pow(pi(), 4));
-        CHECK(_1 / 945 == dzetaFunction(6) / pow(pi(), 6));
-        CHECK(_1 / 9450 == dzetaFunction(8) / pow(pi(), 8));
-        CHECK(_1 / 93550 == dzetaFunction(10) / pow(pi(), 10));
-        CHECK(_691 / 638512875 == dzetaFunction(12) / pow(pi(), 12));
-        CHECK(_2 / 18243225 == dzetaFunction(14) / pow(pi(), 14));
-        CHECK(_3617 / 325641566250 == dzetaFunction(16) / pow(pi(), 16));
-        CHECK(_43867 / 38979295480125 == dzetaFunction(18) / pow(pi(), 18));
-        CHECK(_174611 / 1531329465290625 == dzetaFunction(20) / pow(pi(), 20));
-        CHECK(_77683 / 13447856940643124 == dzetaFunction(22) / pow(pi(), 22));
-        CHECK(_236364091 / BmpReal(201919571963756511232.0) == dzetaFunction(24) / pow(pi(), 24));
+        CHECK(_1 / 6 == zetaFunction(2) / pow(pi(), 2));
+        CHECK(_1 / 90 == zetaFunction(4) / pow(pi(), 4));
+        CHECK(_1 / 945 == zetaFunction(6) / pow(pi(), 6));
+        CHECK(_1 / 9450 == zetaFunction(8) / pow(pi(), 8));
+        CHECK(_1 / 93550 == zetaFunction(10) / pow(pi(), 10));
+        CHECK(_691 / 638512875 == zetaFunction(12) / pow(pi(), 12));
+        CHECK(_2 / 18243225 == zetaFunction(14) / pow(pi(), 14));
+        CHECK(_3617 / 325641566250 == zetaFunction(16) / pow(pi(), 16));
+        CHECK(_43867 / 38979295480125 == zetaFunction(18) / pow(pi(), 18));
+        CHECK(_174611 / 1531329465290625 == zetaFunction(20) / pow(pi(), 20));
+        CHECK(_77683 / 13447856940643124 == zetaFunction(22) / pow(pi(), 22));
+        CHECK(_236364091 / BmpReal(201919571963756511232.0) == zetaFunction(24) / pow(pi(), 24));
     }
 }
