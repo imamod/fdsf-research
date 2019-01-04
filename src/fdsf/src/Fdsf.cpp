acabdb0aca38e32410e379d86c8de53aa21b7335
semenak94 <semenak94@mail.ru>
2019-01-04 12:47:23 +0300

    Поддерживаемые индексы перенесены в заголовочный файл

--- src/fdsf/src/Fdsf.cpp
+++ src/fdsf/src/Fdsf.cpp
@@ -9,24 +9,6 @@
 #include "Constants.h"
 #include "JsonFields.h"
 
-// TODO: вынести в отдельный модуль
-namespace fdsf {
-    /* Поддерживаемые индексы функций ФД */
-    namespace index {
-        const BmpReal M3_HALF = -3.0 / 2;
-        const BmpReal M1_HALF = -1.0 / 2;
-        const BmpReal ZERO = 0;
-        const BmpReal P1_HALF = 1.0 / 2;
-        const BmpReal P1 = 1;
-        const BmpReal P3_HALF = 3.0 / 2;
-        const BmpReal P2 = 2;
-        const BmpReal P5_HALF = 5.0 / 2;
-        const BmpReal P3 = 3;
-        const BmpReal P7_HALF = 7.0 / 2;
-        const BmpReal P4 = 4;
-    }
-}
-
 namespace {
     // Вычисление ФД полуцелых индексов
     BmpReal calculateHalf(BmpReal x, BmpReal k) {
