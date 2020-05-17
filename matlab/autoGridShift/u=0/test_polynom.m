cab18630c98665ce039c88e45cdc233a502994b0
semenak94 <semenak94@mail.ru>
2020-05-17 17:02:33 +0300

    Aлгоритм автоматического выравнивания узлов, данные для примера u = 0

--- matlab/autoGridShift/u=0/test_polynom.m
+++ matlab/autoGridShift/u=0/test_polynom.m
@@ -4,11 +4,15 @@ close all
 
 format long
 %N = 15; % Рабочее N, для которого строим графики 1 - 6
-N_arr=15:-3:3;
+N_arr=15%3:15;
 iterations=[];
 L_first_p1 = [];
 L_neighb_last = [];
-for N = 15:-3:3 % Диагностика зависимости числа итераций от N
+%%%
+min_ekstr_it = [];
+max_ekstr_it = [];
+final_ekstr_it = [];
+for N = N_arr % Диагностика зависимости числа итераций от N
 % Равномерное распределение точек
 n = 0:N;
 x = (2.*n-N)/N;
@@ -19,6 +23,12 @@ maxIterCount = 50;
 U_RELATION_CONST = 0.01;
 L = [];
 it = 1;
+% Профиль погрешности за 
+first_it_profile = [];
+first_it_ekstr = [];
+first_it_X = [];
+last_it_profile = [];
+last_it_X = [];
 while it < maxIterCount
     disp('Итерация: '); disp(it);
     % Дополнительные узлы
@@ -37,8 +47,18 @@ while it < maxIterCount
     % Получаем экстремумы
     clear ekstrDelta; ekstrDelta = ekstremList(U, N);
     L = [L, max(abs(ekstrDelta))/min(abs(ekstrDelta)) - 1];
+    if it == 1
+      %  min_ekstr_it = [min_ekstr_it, min(abs(ekstrDelta))];
+      %  max_ekstr_it = [max_ekstr_it, max(abs(ekstrDelta))];
+        first_it_profile = U;
+        first_it_ekstr = ekstrDelta;
+        first_it_X = X;
+    end
     % Критерий останова
     if L(end) < U_RELATION_CONST
+        last_it_profile = U;
+        last_it_X = X;
+      %  final_ekstr_it = [final_ekstr_it, max(abs(ekstrDelta))];
         break;
     end
     x = shiftGridAccurate(x, ekstrDelta); 
@@ -49,8 +69,8 @@ end
 
 disp('L = max|e|/min|e| - 1');
 disp('L: '); disp(L');
-%lg_max_div_min = log10(max_div_min);
-%disp('lg(L): '); disp(lg_max_div_min');
+lg_L = log10(L);
+disp('lg(L): '); disp(lg_L');
 disp('-----------------');
 neighbours = [];
 for i=1:length(L)-1
@@ -68,3 +88,8 @@ end
 
 t = table(N_arr', iterations, L_first_p1, L_neighb_last, 'VariableNames',{'N','it', 'L_plus_1', 'L_neighb_last'});
 disp(t);
+
+if not isempty(min_ekstr_it)
+    ekstr = table(N_arr', min_ekstr_it', max_ekstr_it', final_ekstr_it', 'VariableNames',{'N', 'min_ekstr_it', 'max_ekstr_it', 'final_ekstr_it'});
+    disp(ekstr);
+end
